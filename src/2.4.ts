3c41aca165705a63e449a5f46cd08e79a511c8bb
Radiat09 <rzsianboyridoy10@gmail.com>
2025-05-03 16:31:38 +0600

    Generic type with Interfaces in TS: M2-2.4

--- src/2.4.ts
+++ src/2.4.ts
@@ -1,3 +1,65 @@
 {
-  //
+  //*Generic Type with Interfaces
+
+  interface Developer<T, X = null> {
+    name: string;
+    computer: {
+      brand: string;
+      model: string;
+      releaseYear: number;
+    };
+    smartWatch: T;
+    bike?: X;
+  }
+
+  type EmailabWatch = {
+    brand: string;
+    model: string;
+    display: string;
+  };
+
+  const poorDeveloper: Developer<EmailabWatch> = {
+    name: "Persian",
+    computer: {
+      brand: "Apple",
+      model: "MacBook Pro",
+      releaseYear: 2022,
+    },
+    smartWatch: {
+      brand: "Apple",
+      model: "Apple Watch Series 7",
+      display: "OLED",
+    },
+  };
+
+  interface appleWatch {
+    brand: string;
+    model: string;
+    heartMonitor: boolean;
+    sleepMonitor: boolean;
+  }
+
+  interface bike {
+    model: string;
+    engine: string;
+  }
+
+  const richDeveloper: Developer<appleWatch, bike> = {
+    name: "Richer",
+    computer: {
+      brand: "Apple",
+      model: "MacBook Pro",
+      releaseYear: 2022,
+    },
+    smartWatch: {
+      brand: "Apple",
+      model: "Apple Watch Series 7",
+      heartMonitor: true,
+      sleepMonitor: true,
+    },
+    bike: {
+      model: "yamaha",
+      engine: "electric",
+    },
+  };
 }
